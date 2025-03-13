fe469eebfa903c296ca234e885bf24c76d350584
Siddhant Nigam <codewithsiddhant777@gmail.com>
2025-03-13 15:15:15 +0530

    first commit

---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.LoginPageLoc;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    void setup() {
+        driver = new ChromeDriver();
+        String baseUrl = "https://stage.nurture.farm/reward-points";
+        driver.get(baseUrl);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+    @Test
+    void testLogin()
+    {
+        LoginPageLoc lpo = new LoginPageLoc(driver);
+        lpo.setPhoneNumber("9425790844");
+        lpo.clickSendOtp();
+        lpo.enterOtp1("1");
+        lpo.enterOtp2("0");
+        lpo.enterOtp3("0");
+        lpo.enterOtp4("0");
+        lpo.clickLogin();
+    }
+
+    @AfterClass
+    void teardown(){
+        driver.quit();
+    }
+}
+
