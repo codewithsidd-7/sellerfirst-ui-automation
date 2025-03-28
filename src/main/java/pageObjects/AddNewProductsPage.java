43fb92fca52fb10d00062d269c4ea27375cf3594
Siddhant Nigam <codewithsiddhant777@gmail.com>
2025-03-28 16:45:51 +0530

    AddNewProducts-Half

---
+++ src/main/java/pageObjects/AddNewProductsPage.java
@@ -0,0 +1,135 @@
+package pageObjects;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import utils.FetchProperties;
+
+public class AddNewProductsPage extends BasePage {
+    public AddNewProductsPage (WebDriver driver){
+    super(driver);
+    }
+
+    //Locators
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav/div/mat-nav-list/app-menu-list-item/div[3]")
+    private WebElement btn_cat_loc;
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav/div/mat-nav-list/app-menu-list-item/div[3]/div/a[1]/span/div")
+    private WebElement btn_mastercat_loc;
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav-content/div/div/div[2]/master-catalogue/div/div/div/mat-card[2]/div[2]/button")
+    private WebElement btn_showall_loc;
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav-content/div/div/div[2]/master-catalogue/div/div/div/mat-card[1]/div[2]/button[3]")
+    private WebElement btn_catimport_loc;
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div/mat-dialog-container/master-catalogue-import-modal/div/div[2]/form/div/div/upload-media/input")
+    private WebElement btn_uploadcatfile_loc;
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div/mat-dialog-container/master-catalogue-import-modal/div/div[2]/form/mat-dialog-actions/div/button[2]")
+    private WebElement btn_importcatfile_loc;
+    @FindBy(xpath = "/html/body/div[2]/div/div/snack-bar-container/div/div/simple-snack-bar/span")
+    private WebElement popup_confirmation_loc;
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav-content/div/div/div[2]/master-catalogue/div/div/div/mat-card[1]/div[1]/div/div/div/div/div[1]/input")
+    private WebElement fld_search_loc;
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav-content/div/div/div[2]/master-catalogue/div/div/div/mat-card[1]/div[1]/div/div/div/div/div[2]/button")
+    private WebElement btn_search_loc;
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav-content/div/div/div[2]/master-catalogue/div/div/div/mat-card[2]/div/table/tbody/tr/td[1]/mat-checkbox/label")
+    private WebElement cbx_checkbox_loc;
+    @FindBy(xpath = "/html/body/anms-root/div/mat-sidenav-container/mat-sidenav-content/div/div/div[2]/master-catalogue/div/div/div/mat-card[1]/div[2]/button[1]")
+    private WebElement btn_addtomycat_loc;
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div/mat-dialog-container/add-product-catalogue-modal/section/div/section/form/div/div/div/div/ng-select[1]/div/div/div[2]/input")
+    private WebElement slt_enterprise_loc;
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div/mat-dialog-container/add-product-catalogue-modal/section/div/section/form/div/div/div/div/ng-select[2]/div/div/div[2]/input")
+    private WebElement slt_plant_loc;
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div/mat-dialog-container/add-product-catalogue-modal/section/div/mat-dialog-actions/div/button[2]")
+    private WebElement btn_addproduct_loc;
+    @FindBy(xpath = "/html/body/div[2]/div[2]/div/mat-dialog-container/add-product-catalogue-modal/section/div/div")
+    private WebElement div_outclick_loc;
+    @FindBy(xpath = "/html/body/div[2]/div/div/snack-bar-container/div/div/simple-snack-bar/span")
+    private WebElement popup_addentandplant_loc;
+
+
+
+
+
+    //Action methods
+    public void clickCatalogue() {
+        btn_cat_loc.click();
+    }
+
+    public void clickMasterCatalogue() {
+        btn_mastercat_loc.click();
+    }
+
+    public void clickShowAll() {
+        btn_showall_loc.click();
+    }
+
+    public void clickCatalogueImport() {
+        btn_catimport_loc.click();
+    }
+
+    public void uploadMasterCatalogueFile() {
+        String relative_path = properties.getProperty("crop_protect_master_catalogue_path");
+        String catalogue_import= FetchProperties.fetchAbsoluteFilePath(relative_path);
+
+
+        btn_uploadcatfile_loc.sendKeys(catalogue_import);
+    }
+
+    public void clickUploadCatalogueImport() {
+        btn_importcatfile_loc.click();
+    }
+
+    public void popupConfirmation(){
+
+        if (popup_confirmation_loc.isDisplayed()){
+            System.out.println("Pop-up is displayed with the following : " + popup_confirmation_loc.getText());
+        }
+        else {
+            System.out.println("Pop-up is not displayed with the following : " + popup_confirmation_loc.getText());
+        }
+
+    }
+
+    public void enterProductInSearch(){
+        fld_search_loc.sendKeys("Shelby");
+    }
+
+    public void clickSearchButton() {
+        btn_search_loc.click();
+    }
+
+    public void clickTickBox() {
+        cbx_checkbox_loc.click();
+    }
+
+    public void clickAddProductsToMyCatalogue() {
+        btn_addtomycat_loc.click();
+    }
+
+    public void selectEnterprise(){
+        slt_enterprise_loc.sendKeys("Godrej Agrovet Limited");
+        slt_enterprise_loc.sendKeys(Keys.ENTER);
+    }
+
+    public void selectPlant()  {
+        slt_plant_loc.sendKeys("G002");
+        slt_plant_loc.sendKeys(Keys.ENTER);
+
+    }
+
+    public void clickAddProducts() {
+        div_outclick_loc.click();
+        btn_addproduct_loc.click();
+    }
+    public void addEnterpriseAndPlantConfirmation(){
+
+        if(popup_addentandplant_loc.isDisplayed()){
+            System.out.println("Pop-up is Displayed with the following : " + popup_addentandplant_loc.getText());
+        }
+        else{
+            System.out.println("Pop-up is Displayed with the following : " + popup_addentandplant_loc.getText());
+        }
+
+    }
+
+
+}
---
+++ src/test/java/tests/AddNewProductsTest.java
@@ -0,0 +1,88 @@
+package tests;
+
+import baseTest.BaseClass;
+import org.testng.annotations.Test;
+import pageObjects.AddNewProductsPage;
+import pageObjects.LoginPage;
+
+public class AddNewProductsTest extends BaseClass {
+
+    @Test(priority = 1)
+    void login() {
+        LoginPage lpo = new LoginPage(accesing());
+        lpo.setPhoneNumber("9425790844");
+        lpo.clickSendOtp();
+        lpo.enterOtp("1000");
+        lpo.clickLogin();
+    }
+
+    @Test(priority = 2)
+    void openMasterCatalogue() throws InterruptedException {
+        AddNewProductsPage anpp = new AddNewProductsPage(accesing());
+        anpp.clickCatalogue();
+        anpp.clickMasterCatalogue();
+        Thread.sleep(2000);
+        anpp.clickShowAll();
+    }
+
+    @Test(priority = 3)
+    void uploadMasterCatalogueFile() {
+        AddNewProductsPage anpp = new AddNewProductsPage(accesing());
+        anpp.clickCatalogueImport();
+        anpp.uploadMasterCatalogueFile();
+        anpp.clickUploadCatalogueImport();
+        anpp.popupConfirmation();
+    }
+
+
+    @Test(priority = 4)
+    void searchingAddedProduct() throws InterruptedException {
+        AddNewProductsPage anpp = new AddNewProductsPage(accesing());
+        anpp.enterProductInSearch();
+        anpp.clickSearchButton();
+        Thread.sleep(2000);
+    }
+
+    @Test(priority = 5)
+    void addingProductToMyCatalogue() throws InterruptedException {
+        AddNewProductsPage anpp = new AddNewProductsPage(accesing());
+        anpp.clickTickBox();
+        anpp.clickAddProductsToMyCatalogue();
+        anpp.selectEnterprise();
+        Thread.sleep(1000);
+        anpp.selectPlant();
+        anpp.clickAddProducts();
+        anpp.addEnterpriseAndPlantConfirmation();
+    }
+
+    @Test(priority = 6)
+    void openMyCatalogue() throws InterruptedException {
+        driver.get("https://stage.nurture.farm/my-catalogue");
+        Thread.sleep(3000);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
