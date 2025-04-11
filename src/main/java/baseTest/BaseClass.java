49e5439655ed8c804101179d0f2ba626214317b2
Siddhant Nigam <codewithsiddhant777@gmail.com>
2025-04-11 11:51:52 +0530

    added logs

--- src/main/java/baseTest/BaseClass.java
+++ src/main/java/baseTest/BaseClass.java
@@ -1,5 +1,7 @@
 package baseTest;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,6 +17,7 @@ import static pageObjects.BasePage.properties;
 public class BaseClass {
 
     public WebDriver driver;
+    public Logger logger;
     private LoginPage loginPage;
     public BaseClass(){
         driver=new ChromeDriver();
@@ -22,9 +25,9 @@ public class BaseClass {
 
 
 
-
     @BeforeClass
     public void setup() {
+        logger= LogManager.getLogger(this.getClass()); //log4j2
         String baseUrl = "https://stage.nurture.farm/reward-points";
         driver.get(baseUrl);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
--- src/test/java/tests/RemoveRewardPointsTest.java
+++ src/test/java/tests/RemoveRewardPointsTest.java
@@ -11,28 +11,68 @@ public class RemoveRewardPointsTest extends BaseClass {
 
     @Test(priority = 1)
     void openRewards() {
+        logger.info("Starting test: openRewards");
+
         rrpp.clickRetailers();
+        logger.debug("Clicked on 'Retailers'");
+
         rrpp.clickRewards();
+        logger.debug("Clicked on 'Rewards'");
+
+        logger.info("Finished test: openRewards");
     }
 
     @Test(priority = 2)
     void searchRetailer() {
+        logger.info("Starting test: searchRetailer");
+
         rrpp.enterRetailerNumber();
+        logger.debug("Entered retailer number");
+
         rrpp.searchRetailer();
+        logger.debug("Searched for retailer");
+
         rrpp.clickEditRetailer();
+        logger.debug("Clicked on 'Edit Retailer'");
+
+        logger.info("Finished test: searchRetailer");
     }
+
     @Test(priority = 3)
     void removeRewardPoints() {
+        logger.info("Starting test: removeRewardPoints");
+
         rrpp.selectRemovePoints();
+        logger.debug("Selected 'Remove Points'");
+
         rrpp.selectReasonDropdown();
+        logger.debug("Opened reason dropdown");
+
         rrpp.selectReason();
+        logger.debug("Selected reason for point removal");
+
         rrpp.addPointsValue();
+        logger.debug("Entered points value");
+
         rrpp.clickConfirmButton();
+        logger.debug("Clicked on 'Confirm'");
+
         rrpp.clickYesButton();
+        logger.debug("Confirmed removal with 'Yes'");
+
+        logger.info("Finished test: removeRewardPoints");
     }
+
     @Test(priority = 4)
     void validateConfirmationMessage() {
+        logger.info("Starting test: validateConfirmationMessage");
+
         rrpp.validatePopupMessage();
+        logger.debug("Validated confirmation popup message");
+
+        logger.info("Finished test: validateConfirmationMessage");
     }
 }
+
+
 
